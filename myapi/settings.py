ff23d306f2f1ecfb2c989aa53ee67c7985106595
bling11 <ksambling@gmail.com>
2025-04-16 16:01:02 +0800

    Initial commit

--- myapi/settings.py
+++ myapi/settings.py
@@ -54,6 +54,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # should come just after SecurityMiddleware
+    'django.middleware.security.SecurityMiddleware',
 ]
 
 ROOT_URLCONF = 'myapi.urls'
