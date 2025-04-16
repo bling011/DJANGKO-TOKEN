2d041fb3bf161952c3a36736f297b457004ffcdc
bling11 <ksambling@gmail.com>
2025-04-16 16:40:56 +0800

    Initial commit

--- myapi/settings.py
+++ myapi/settings.py
@@ -1,5 +1,12 @@
 from pathlib import Path
 import os
+import dj_database_url
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
+}
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
